5c3cf79edd22c68718792fd54f35ebc62e08070e
Victor Sabino <victorsabino6@gmail.com>
2021-10-03 19:13:53 -0300

    1.0.0

--- src/dist/render.dev.js
+++ src/dist/render.dev.js
@@ -1,15 +1,40 @@
 "use strict";
 
-var videoElement = document.querySelector('vide');
-var buttonStart = document.getElementById('buttonStart');
-var buttonEnd = document.getElementById('buttonEnd');
-var buttonSelector = document.getElementById('buttonSelector');
-
 var _require = require('electron'),
     desktopCapturer = _require.desktopCapturer;
 
 var _require2 = require('@electron/remote'),
-    Menu = _require2.Menu;
+    Menu = _require2.Menu,
+    dialog = _require2.dialog;
+
+var videoElement = document.querySelector('video');
+var buttonStart = document.getElementById('buttonStart');
+var buttonEnd = document.getElementById('buttonEnd');
+var buttonSelector = document.getElementById('buttonSelector');
+var errorMessage = document.getElementById('error');
+
+var _require3 = require('fs'),
+    writeFile = _require3.writeFile;
+
+var mediaRecorder = undefined;
+var recordedChuncks = [];
+
+buttonStart.onclick = function (e) {
+  if (!mediaRecorder) {
+    errorMessage.classList.remove('hide');
+    return;
+  }
+
+  mediaRecorder.start();
+  buttonStart.classList.add('is-danger');
+  buttonStart.innerText = 'Recording';
+};
+
+buttonEnd.onclick = function (e) {
+  mediaRecorder.stop();
+  buttonStart.classList.remove('is-danger');
+  buttonStart.innerText = 'Start';
+};
 
 function getVideoSources() {
   var inputSources, videosOptionsMenu;
@@ -25,7 +50,6 @@ function getVideoSources() {
         case 2:
           inputSources = _context.sent;
           videosOptionsMenu = Menu.buildFromTemplate(inputSources.map(function (source) {
-            console.log('source ', source);
             return {
               label: source.name,
               click: function click() {
@@ -39,6 +63,93 @@ function getVideoSources() {
         case 6:
         case "end":
           return _context.stop();
+      }
+    }
+  });
+}
+
+function selectSource(source) {
+  var options, stream;
+  return regeneratorRuntime.async(function selectSource$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          if (!errorMessage.classList.contains('hide')) {
+            errorMessage.classList.add('hide');
+          }
+
+          buttonSelector.innerText = source.name;
+          options = {
+            audio: false,
+            video: {
+              mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+              }
+            }
+          };
+          _context2.next = 5;
+          return regeneratorRuntime.awrap(navigator.mediaDevices.getUserMedia(options));
+
+        case 5:
+          stream = _context2.sent;
+          videoElement.srcObject = stream;
+          videoElement.play();
+          mediaRecorder = new MediaRecorder(stream, {
+            mimeType: 'video/webm; codecs=vp9'
+          });
+          mediaRecorder.ondataavailable = handleDataAvailable;
+          mediaRecorder.onstop = handleStop;
+
+        case 11:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+}
+
+function handleDataAvailable(e) {
+  console.log('chunck video ', recordedChuncks);
+  recordedChuncks.push(e.data);
+}
+
+function handleStop(e) {
+  var blob, buffer, _ref, filePath;
+
+  return regeneratorRuntime.async(function handleStop$(_context3) {
+    while (1) {
+      switch (_context3.prev = _context3.next) {
+        case 0:
+          blob = new Blob(recordedChuncks, {
+            type: 'video/webm; codecs=vp9'
+          });
+          _context3.t0 = Buffer;
+          _context3.next = 4;
+          return regeneratorRuntime.awrap(blob.arrayBuffer());
+
+        case 4:
+          _context3.t1 = _context3.sent;
+          buffer = _context3.t0.from.call(_context3.t0, _context3.t1);
+          _context3.next = 8;
+          return regeneratorRuntime.awrap(dialog.showSaveDialog({
+            buttonLabel: 'Save video',
+            defaultPath: "vid-".concat(Date.now(), ".webm")
+          }));
+
+        case 8:
+          _ref = _context3.sent;
+          filePath = _ref.filePath;
+
+          if (filePath) {
+            writeFile(filePath, buffer, function () {
+              return console.log('video saved successfully!');
+            });
+          }
+
+        case 11:
+        case "end":
+          return _context3.stop();
       }
     }
   });
--- src/render.js
+++ src/render.js
@@ -1,18 +1,40 @@
+const { desktopCapturer  } = require('electron')
+const { Menu, dialog } = require('@electron/remote');
 
-const videoElement = document.querySelector('vide')
+
+const videoElement = document.querySelector('video')
 const buttonStart = document.getElementById('buttonStart')
 const buttonEnd = document.getElementById('buttonEnd')
 const buttonSelector = document.getElementById('buttonSelector')
-const { desktopCapturer  } = require('electron')
-const { Menu } = require('@electron/remote');
+const errorMessage = document.getElementById('error')
+const { writeFile } = require('fs');
+
+let mediaRecorder = undefined;
+let recordedChuncks = [];
+
+buttonStart.onclick = e => {
+  if (!mediaRecorder) {
+    errorMessage.classList.remove('hide')
+    return
+  }
+  mediaRecorder.start();
+  buttonStart.classList.add('is-danger');
+  buttonStart.innerText = 'Recording';
+};
+
+buttonEnd.onclick = e => {
+  mediaRecorder.stop();
+  buttonStart.classList.remove('is-danger');
+  buttonStart.innerText = 'Start';
+};
 
 async function getVideoSources () {
   const inputSources = await desktopCapturer.getSources({
     types: ['window', 'screen']
   });
+
   const videosOptionsMenu = Menu.buildFromTemplate(
     inputSources.map(source => {
-      console.log('source ', source)
       return {
         label: source.name,
         click: () => selectSource(source)
@@ -21,6 +43,53 @@ async function getVideoSources () {
   )
   console.log('videosOptionsMenu ', videosOptionsMenu)
   videosOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+  if (!errorMessage.classList.contains('hide')) {
+    errorMessage.classList.add('hide');
+  }
+  buttonSelector.innerText = source.name;
+  const options =  {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id,
+      }
+    }
+  };
+
+  const stream = await navigator.mediaDevices.getUserMedia(options);
+  videoElement.srcObject = stream;
+  videoElement.play();
+  mediaRecorder = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=vp9'});
+
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+
+}
+
+function handleDataAvailable(e) {
+  console.log('chunck video ', recordedChuncks);
+  recordedChuncks.push(e.data);
+}
+
+async function handleStop (e) {
+  const blob = new Blob(recordedChuncks, {
+    type: 'video/webm; codecs=vp9'
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: 'Save video',
+    defaultPath: `vid-${Date.now()}.webm`
+  });
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+  }
 }
 
 
